17c48026d19310ec73d4ed02909130ada00a6a5f
Trong-An Bui <trongan93@gmail.com>
2019-08-30 14:19:19 +0800

    add usgs combine bands

---
+++ usgs-combine-bands.py
@@ -0,0 +1,5 @@
+### Description:
+### Read the bands of satellite land image and combine to RGB image. Depend on each Landsat, use the suitable bands todo. 
+### Follow at link: https://www.usgs.gov/faqs/what-are-band-designations-landsat-satellites?qt-news_science_products=0#qt-news_science_products
+### First open by trongan93 on Aug 30th 2019
+### Last modify by ..........
