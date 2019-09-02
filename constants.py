df1d6a6026e83fd9d56be5d070f794e2f70a6e2b
Kha Giai Minh <trainer1234.giaiminh@gmail.com>
2019-09-02 21:18:13 +0700

    update config

--- constants.py
+++ constants.py
@@ -1,5 +1,5 @@
-BASE_PATH = '/home/trainer1234/research/Data/Landsat_Data'
-METADATA_CATALOG = '/home/trainer1234/research/Data/Landsat_Data/metadata-catalog'
-WRS_SHAPE_FILE_PATH = '/home/trainer1234/research/AILandImage/AILandimage/wrs2_shapefiles/wrs2_descending.shp'
+BASE_PATH = '/media/trongan93/Data/ProjectData/Landsat'
+METADATA_CATALOG = '/media/trongan93/Data/ProjectData/Landsat/metadata-catalog'
+WRS_SHAPE_FILE_PATH = '/home/trongan93/Projects/AILandimage/wrs2_shapefiles/wrs2_descending.shp'
 DATASET_TYPE = ['LC8', 'LE7', 'LT5']
 FILTER = ['CLOUDCOVER']
