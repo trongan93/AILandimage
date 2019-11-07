30fd62c62ebf5708d368591a0a311340cf3fee86
Trong-An Bui <trongan93@gmail.com>
2019-11-07 16:19:49 +0800

    update path

--- constants.py
+++ constants.py
@@ -1,8 +1,8 @@
-DOWNLOADED_BASE_PATH = '/media/trongan93/Data/ProjectData/Landsat'
-IMAGE_BASE_PATH = '/media/trongan93/Data/ProjectData/Landsat/Images'
-INPUT_FILE_PATH = '/home/trongan93/Projects/AILandimage/input.csv'
-WRS_SHAPE_FILE_PATH = '/home/trongan93/Projects/AILandimage/wrs2_shapefiles/wrs2_descending.shp'
-USGS_CREDENTIAL_FILE = '/home/trongan93/Projects/AILandimage/usgs.txt'
+DOWNLOADED_BASE_PATH = '/mnt/2A967D2D967CFB21/ProjectData/Landsat'
+IMAGE_BASE_PATH = '/mnt/2A967D2D967CFB21/ProjectData/Landsat/Images'
+INPUT_FILE_PATH = 'input.csv'
+WRS_SHAPE_FILE_PATH = 'wrs2_shapefiles/wrs2_descending.shp'
+USGS_CREDENTIAL_FILE = 'usgs.txt'
 
 DATASET_TYPE = ['LC8', 'LE7', 'LT5']
 FILTER = ['CLOUDCOVER']
