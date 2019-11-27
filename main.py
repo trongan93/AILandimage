0dad70f44ada2ecf33a7966d0375b935f321e088
Trong-An Bui <trongan93@gmail.com>
2019-11-27 14:02:54 +0800

    update requirements in main file

--- main.py
+++ main.py
@@ -28,6 +28,12 @@ def main():
         #     writer = csv.writer(backup)
         #     writer.writerow(line.values())
 
+        ### Created by trongan93 Nov 27th
+        ### Read csv file after downloaded, check field size and call crop_image.py function. Central of crop_image is lat,lng, size of crop image is depended on size in input file.
+        
+        
+
+
 
 if __name__ == "__main__":
     main()  
