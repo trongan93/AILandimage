5b8ecc5dc64eb84a8d093425a759979d5cbdc274
Kha Giai Minh <trainer1234.giaiminh@gmail.com>
2019-12-01 14:53:15 +0700

    add directory helper

---
+++ utilities/directory_helper.py
@@ -0,0 +1,14 @@
+import os
+
+def split_all(path):
+    folders = []
+    while 1:
+        path, folder = os.path.split(path)
+        if folder != "":
+            folders.append(folder)
+        else:
+            if path != "":
+                folders.append(path)
+            break
+        
+    return folders
