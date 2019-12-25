b17f06936f314863f23752d9a7c5807f8ec409c2
trongan93 <trongan93@gmail.com>
2019-12-25 10:51:22 +0800

    update combine band

--- test_combine_bands.py
+++ test_combine_bands.py
@@ -1,10 +1,13 @@
 from utilities.image_pre import *
 import matplotlib.pyplot as plt
+import cv2
 
 # path_imgs_folder = "/mnt/d/ProjectData/Landsat/Images/040034/2019-08-11/LC08/CLOUDCOVER"
 path_imgs_folder = "/mnt/d/ProjectData/Landsat/tmp/LC08_L1TP_117043_20191213_20191213_01_RT"
 rgb_img = combine_bands(path_imgs_folder)
 plt.imshow(rgb_img)
 plt.show()
-print(rgb_img)
+# cv2.imshow("Output",rgb_img)
+# cv2.waitKey(0)
+# print(rgb_img)
 
--- utilities/image_pre.py
+++ utilities/image_pre.py
@@ -1,10 +1,15 @@
 import os
 import cv2
 import matplotlib.pyplot as plt
+import numpy as np
+import rasterio
 
 def combine_bands(imgs_folder_path):
     imgs = load_images_from_folder(imgs_folder_path)
     return imgs
+def read_img_by_rasterio(folder, filename):
+    dataset = rasterio.open(os.path.join(folder,filename))
+    return dataset
 
 def load_images_from_folder(folder):
     global blue_img, green_img, red_img
@@ -12,21 +17,34 @@ def load_images_from_folder(folder):
         band_name = file_name_band(filename)
         if band_name == "B2":
             blue_img = cv2.imread(os.path.join(folder,filename),0)
+            blue_np = np.fromfile(os.path.join(folder,filename),dtype='uint8')
+            blue_dataset = read_img_by_rasterio(folder, filename)
+            print("blue dataset witdth: ", blue_dataset.width)
+            print("blue dataset transform: \n", blue_dataset.transform)
+            print("blue dataset bound: ", blue_dataset.bounds)
+            print("Coordinate reference system (CRS): ", blue_dataset.crs)
+            print("dataset indexes: ", blue_dataset.indexes)
+
         elif band_name == "B3":
             green_img = cv2.imread(os.path.join(folder,filename),0)
+            green_np = np.fromfile(os.path.join(folder,filename),dtype='uint8')
+            green_dataset = read_img_by_rasterio(folder, filename)
         elif band_name == "B4":
             red_img = cv2.imread(os.path.join(folder,filename),0)
+            red_np = np.fromfile(os.path.join(folder,filename),dtype='uint8')
+            red_dataset = read_img_by_rasterio(folder, filename)
+
     # if(blue_img == green_img):
     #     print("blue img and green img is equally")
-    plt.imshow(blue_img,cmap="gray")
-    plt.title("Blue channel - B2")
-    plt.show()
-    plt.imshow(green_img,cmap="gray")
-    plt.title("Green channel - B3")
-    plt.show()
-    plt.imshow(red_img,cmap="gray")
-    plt.title("Red channel - B4")
-    plt.show()
+    # plt.imshow(blue_img,cmap="gray")
+    # plt.title("Blue channel - B2")
+    # plt.show()
+    # plt.imshow(green_img,cmap="gray")
+    # plt.title("Green channel - B3")
+    # plt.show()
+    # plt.imshow(red_img,cmap="gray")
+    # plt.title("Red channel - B4")
+    # plt.show()
     rgb_img = cv2.merge([red_img,green_img,blue_img])
     return rgb_img
 
