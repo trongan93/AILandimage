1015a69bf2faf1ca3695595eff58d33cc3d3482a
trongan93 <trongan93@gmail.com>
2020-04-07 17:20:55 +0800

    update path

--- build_dataset.py
+++ build_dataset.py
@@ -3,7 +3,7 @@ def change_path_in_dataserver(downloaded_img_path):
     if(downloaded_img_path != None and downloaded_img_path != '' and downloaded_img_path != 'NODATA'):
         path_members = downloaded_img_path.split('/')
         # print('old path: ', downloaded_img_path)
-        new_path = path_members[0] + '/' + path_members[1] + '/' + path_members[2] + '/' + path_members[4] + '/' + path_members[5] + '/' + path_members[6] + '/' + path_members[7] + '/' + path_members[8] + '/' + path_members[9] + '/' + 'cropped'
+        new_path = path_members[0] + '/' + path_members[1] + '/' + path_members[2] + '/' + path_members[4] + '/' + path_members[5] + '/' + path_members[6] + '/' + path_members[7] + '/' + path_members[8] + '/' + path_members[9] + '/' + 'cropped/'
         # print('new path: ', new_path)
         return new_path
 
